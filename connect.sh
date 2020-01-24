9d06e75e28d1c8170ed480b63777d6315b066b5e
rhine59 <rhine59@gmail.com>
2020-01-24 16:27:49 +0000

    Jan24
    Signed-off-by: rhine59 <rhine59@gmail.com>

--- connect.sh
+++
@@ -1,4 +0,0 @@
-echo password for ansible tower users is alpine
-echo help vijay
-echo user1.user50 alpine-has-acorn-valley on 169.45.111.84 same private_key
-ssh -i private_key root@169.62.229.236
--- target.sh
+++
@@ -1,3 +0,0 @@
-echo this connects to the machine we are going to be running NGINX on.
-echo variables port name and userid
-ssh -i private_key root@169.62.229.200
